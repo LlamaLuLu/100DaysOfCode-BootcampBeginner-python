df22bf5437a99afe25736dbec83508b7f81bd59e
Lulama Lingela <lulamahana@gmail.com>
2023-06-18 23:14:12 +0200

    Create fizz_buzz.py

---
+++ fizz_buzz.py
@@ -0,0 +1,12 @@
+# 9/11/2021
+# fizz buzz game
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
