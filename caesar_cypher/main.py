b09a0e34c316475d14e00107b182be79b2ce5279
Lulama Lingela <lulamahana@gmail.com>
2023-06-18 23:23:49 +0200

    Add files via upload

---
+++ caesar_cypher/main.py
@@ -0,0 +1,35 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+      end_text += char
+    else:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: \n{end_text}")
+
+from art import *
+print(logo)
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+
+continuation = "yes"
+while continuation == "yes":
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  if shift > 26:
+    shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  continuation = input("Do you want to continue? 'Yes' or 'No'\n").lower()
+print("Goodbye")
